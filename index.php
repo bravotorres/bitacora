eb8ce739743fd01da217271d70b8024d1d96db6f
Alejandro Bravo <navegonauta@gmail.com>
2018-11-28 16:07:04 -0600

    Avance

--- index.php
+++ index.php
@@ -9,25 +9,25 @@ date_default_timezone_set('Mexico/General');
     <title>Inicio de sesión</title>
 
     <title>Bitácora - Login</title>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
     <link rel="icon" href="images/ipn.png">
     <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link href="login.css" rel="stylesheet" id="bootstrap-csss">
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+
 </head>
 <body id="LoginForm">
 <div class="container">
     <!-- Panel de Administrador ...-->
-    
 
     <!-- Inicio de Sesión -->
     <div class="login-form">
         <div class="main-div">
             <div class="panel">
                 <h2>Inicio de sesión (index.php)</h2>
-                <p>Para acceder, ingrese su información.</p>
+                <p>Para generar el registro, ingrese su información.</p>
             </div>
 
             <!--Formulario: Autenticación-->
@@ -43,19 +43,21 @@ date_default_timezone_set('Mexico/General');
                 </div>
 
                 <!--Enlace: Contraseña Olvidada...-->
-                <div class="forgot">
+                <!-- <div class="forgot">
                     <a href="#">¿Olvidó su contraseña?</a>
-                </div>
+                </div> -->
 
                 <!--Botón: Iniciar Sesión-->
                 <!-- <button type="submit" class="btn btn-primary">Acceder</button> -->
-                <button type="submit" name="entrada" id="entrada" class="btn btn-success btn-lg">
-                    Entrada
-                </button>
+                <!-- <div class="btn-group"> -->
+                    <button type="submit" name="entrada" id="entrada" class="btn btn-success btn-lg btn-block">
+                        Entrada
+                    </button>
 
-                <button type="sumbit" name="salida" id="salida" class="btn btn-danger btn-lg" >
-                    Salida
-                </button>
+                    <button type="sumbit" name="salida" id="salida" class="btn btn-danger btn-lg btn-block" >
+                        Salida
+                    </button>
+                <!-- </div> -->
             </form>
         </div>
 
@@ -95,116 +97,121 @@ date_default_timezone_set('Mexico/General');
 <!--    </form>-->
 <!---->
 <!--    <div>-->
-<?php
-       include("util.php");
-       if (isset($_POST['entrada'])) {
-           $email = $_POST['name'];
-           $pass = $_POST['pass'];
+    <?php
+    include("util.php");
+    if (isset($_POST['entrada'])) {
+        $email = $_POST['name'];
+        $pass = $_POST['pass'];
 
-           print("$email -> $email");
-           print("$pass -> $pass");
+        // printf("email -> $email <br> pass -> $pass");
 
-           $ctaUsuario = "SELECT date(now()) as fhoy, now() as ahora,  idusuarios, username, password FROM usuarios where  idstatus = 1 AND email='$email' AND password='$pass'";
-           $result = $conn->query($ctaUsuario);
-           $rows = $result->num_rows;
+        $ctaUsuario = "SELECT date(now()) as fhoy, now() as ahora,  id, username, password FROM usuarios where  id_status = 1 AND email='$email' AND password='$pass'";
+        
+        $result = $conn->query($ctaUsuario);
+        $rows = $result->num_rows;
 
-           if ($rows==1){
-               for ($i=0; $i <$rows ; $i++) {
-                   $result->data_seek($i);
-                   $row=$result->fetch_array(MYSQLI_ASSOC);
-                   $datenow=$row['fhoy'];
-                   $datetimenow=$row['ahora'];
-                   $idusuario=$row['idusuarios'];
-                   $name=$row['username'];
-               }
+        if ($rows == 1){
+            for ($i=0; $i < $rows ; $i++) {
+                $result->data_seek($i);
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                $datenow = $row['fhoy'];
+                $datetimenow = $row['ahora'];
+                $idusuario = $row['id'];
+                $name = $row['username'];
+            }
 
-               $ctaRegistro="SELECT date(horadeentrada) as fentrada, horadeentrada, horadesalida FROM registro where idusuarios='$idusuario' and date(horadeentrada)=date(now())";
-               $result=$conn->query($ctaRegistro);
-               $rows=$result->num_rows;
+            $ctaRegistro="SELECT date(hora_entrada) as fentrada, hora_entrada, hora_salida FROM registro where id_usuarios='$idusuario' and date(hora_entrada)=date(now())";
+            
+            $result=$conn->query($ctaRegistro);
+            $rows=$result->num_rows;
 
-               if ($rows>0) {
-                   for ($i=0; $i <$rows ; $i++) {
-                       $result->data_seek($i);
-                       $row=$result->fetch_array(MYSQLI_ASSOC);
-                       $date=$row['fentrada'];
-                       $datetime=$row['horadeentrada'];
-                   }
-                   print("<br><div class='alert alert-danger font' role='alert'>Buen dia $name su hora de entrada ya habia sido registrada: $datetime </div>");
-               }else{
-                   $inserRegistro="INSERT INTO registro VALUES(0,'$idusuario', NOW(), 'null', 'null','null')";
-                   $result=$conn->query($inserRegistro);
-                   print("<br><div class='alert alert-success font' role='alert'>Buen dia $name su hora  de entrada se ha registrado: $datetimenow  ,<a href='deletesession.php'> <b>has click aqui para continuar</b></a></div>");
-               }
+            if ($rows > 0) {
+                for ($i=0; $i <$rows ; $i++) {
+                    $result->data_seek($i);
+                    $row = $result->fetch_array(MYSQLI_ASSOC);
+                    $date = $row['fentrada'];
+                    $datetime = $row['hora_entrada'];
+                }
+                print("<br><div class='alert alert-danger font' role='alert'>Buen dia $name, su hora de entrada ya habia sido registrada: $datetime </div>");
+            }else{
+                $inserRegistro = "INSERT INTO registro (id_usuarios, hora_entrada, hora_salida, maquina, comentario) VALUES ('$idusuario', NOW(), '0001-01-01 00:00:00', 0, '')";
+                print("<br><div class='alert alert-danger' role='alert'>query: $inserRegistro</div>");
+                $result = $conn->query($inserRegistro);
+                print("<br><div class='alert alert-success font' role='alert'>Buen dia $name, su hora  de entrada se ha registrado: $datetimenow  ,<a href='deletesession.php'> <b>has click aqui para continuar</b></a></div>");
+            }
 
-           }else{
-               print("<br><div class='alert alert-danger' role='alert'>Sus datos no coinciden</div>");
-           }
-           $conn->close();
-       }
+        }else{
+            print("<br><div class='alert alert-danger' role='alert'>Sus datos no coinciden</div>");
+        }
+        $conn->close();
+    }
 
-       if (isset($_POST['salida'])) {
+    if (isset($_POST['salida'])) {
+        include("util.php");
+        
+        $email = $_POST['name'];
+        $pass = $_POST['pass'];
 
-           $email=$_POST['name'];
-           $pass=$_POST['pass'];
-           include("util.php");
+        $ctaUsuario = "SELECT date(now()) as fhoy, now()  as ahora, id, username, password FROM usuarios where  id_status = 1 AND email='$email' AND password='$pass'";
+        print("<br><div class='alert alert-danger' role='alert'>ROWS:".$result->num_rows." @@ $ctaUsuario</div>");
+        $result = $conn->query($ctaUsuario);
+        $rows = $result->num_rows;
 
-           $ctaUsuario= "SELECT date(now()) as fhoy, now()  as ahora,  idusuarios, username, password FROM usuarios where  idstatus = 1 AND email='$email' AND password='$pass'";
-           $result=$conn->query($ctaUsuario);
-           $rows=$result->num_rows;
+        if ($rows > 0) {
 
-           if ($rows>0) {
+            session_start();
+            
+            $_SESSION['eMail'] = $email;
+            $_SESSION['Pass'] = $pass;
+            
+            for ($i=0; $i < $rows ; $i++) {
+                $result->data_seek($i);
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                $datenow = $row['fhoy'];
+                $datetimenow = $row['ahora'];
+                $idusuario = $row['id'];
+                $name = $row['username'];
+            }
 
-               session_start();
-               $_SESSION['eMail']=$email;
-               $_SESSION['Pass']=$pass;
-               for ($i=0; $i <$rows ; $i++) {
-                   $result->data_seek($i);
-                   $row=$result->fetch_array(MYSQLI_ASSOC);
-                   $datenow=$row['fhoy'];
-                   $datetimenow=$row['ahora'];
-                   $idusuario=$row['idusuarios'];
-                   $name=$row['username'];
-               }
-               $ctaRegistro="SELECT date(horadeentrada) as fentrada, idregistro, horadeentrada, horadesalida FROM registro where idusuarios='$idusuario' and date(horadeentrada)=date(now())";
-               $result=$conn->query($ctaRegistro);
-               $rows=$result->num_rows;
+            $ctaRegistro = "SELECT date(hora_entrada) as fentrada, id, hora_entrada, hora_salida FROM registro where id_usuarios='$idusuario' and date(hora_entrada)=date(now())";
+            print("<br><div class='alert alert-danger' role='alert'>@@ $ctaRegistro</div>");
+            $result = $conn->query($ctaRegistro);
+            $rows = $result->num_rows;
 
+            if ($rows > 0) {
+                for ($i = 0; $i < $rows ; $i++) {
+                    $result->data_seek($i);
+                    $row = $result->fetch_array(MYSQLI_ASSOC);
+                    $date = $row['fentrada'];
+                    $datetime = $row['hora_entrada'];
+                    $datetimeS = $row['hora_salida'];
+                    $idregistro = $row['id'];
+                }
 
-               if ($rows>0) {
-                   for ($i=0; $i <$rows ; $i++) {
-                       $result->data_seek($i);
-                       $row=$result->fetch_array(MYSQLI_ASSOC);
-                       $date=$row['fentrada'];
-                       $datetime=$row['horadeentrada'];
-                       $datetimeS=$row['horadesalida'];
-                       $idregistro=$row['idregistro'];
-
-                   }
-                   if ($datetimeS=='0000-00-00 00:00:00') {
-                       header("Location: registro.php");
-                   }else{
-                       print("<br><div class='alert alert-danger font' role='alert'>Buen dia $name su hora de salida ya habia sido registrada: $datetimeS </div>");
-                   }
-               }else{
-                   print("<br><div class='alert alert-danger font' role='alert'>Buen dia $name su hora  de entrada no ha sido registrada porfavor registrela para poder registrar su salida</div>");
-               }
-           }else{
-               print("<br><div class='alert alert-danger' role='alert'>Sus datos no coinciden</div>");
-           }
-       }
-?>
+                if($datetimeS == '0001-01-01 00:00:00') {
+                    header("Location: registro.php");
+                    print("<br><div class='alert alert-danger' role='alert'>Location: registro.php</div>");
+                }else{
+                    print("<br><div class='alert alert-danger font' role='alert'>Buen dia $name, su hora de salida ya habia sido registrada: $datetimeS </div>");
+                }
+            }else{
+                print("<br><div class='alert alert-danger font' role='alert'>Buen dia $name, su hora de entrada no ha sido registrada porfavor registrela para poder registrar su salida</div>");
+            }
+        }else{
+            print("<br><div class='alert alert-danger' role='alert'>Sus datos no coinciden</div>");
+        }
+    }
+    ?>
     </div>
     <br>
     <div>
         <a class="btn btn-default btn-lg" href="creacionUsuario.php">
-                <span class="glyphicon glyphicon-user" aria-hidden="true">
-                    <br>Crea usuario
-                </span>
+            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+            <br>Crear Usuario
         </a>
         <a class="btn btn-default btn-lg" href="loginAdmin.php">
-                <span class="glyphicon glyphicon-queen" aria-hidden="true">
-                    <br>Administrador
-                </span>
+            <span class="glyphicon glyphicon-queen" aria-hidden="true"></span>
+            <br>Administrador
         </a>
     </div>
 </div>
--- util.php
+++ util.php
@@ -7,9 +7,16 @@ Design     : P.A.B.L.O
 Programer  : ...""J.A.L.R.""...
 Rev: Alejandro Bravo <quironProject@gmail.com>
 */
+
+// Parámetros  de la Base de Datos:
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'bitacora');
+define('DB_USER', 'root');
+define('DB_PASS', 'Alejandro_1');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 // $conn= mysqli_connect("localhost","root","EsCoMproyecto","login");
-$conn = mysqli_connect("localhost","root","Alejandro_1","bitacora");
-print("<div class='alert alert-success font'>$conn</div>");
+
 if( !$conn ){
     // die('No pudo conectarse.');
      print("<br><div class='alert alert-danger font' role='alert'>No pudo conectarse a la Base de Datos</div>");
